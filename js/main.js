ac22fa22e1fbe930ae634a85c195355c3f74ba8a
crisst <criss.t@northeastern.edu>
2026-05-20 14:31:18 -0400

    Connected local project to GitHub repo

---
+++ js/main.js
@@ -0,0 +1,17 @@
+console.log("Hello world!");
+
+const inTitle = document.querySelector("#input-title");
+const inPrice = document.querySelector("#input-price");
+const inDescription = document.querySelector("#input-description");
+const btnCancel = document.querySelector("#button-cancel");
+
+const onClickCancel = (evt) => {
+    // Don't navigate to a different page
+    evt.preventDefault();
+
+    inTitle.value = "";
+    inPrice.value = 80;
+    inDescription.value = "";
+};
+btnCancel.addEventListener("click", onClickCancel)
+
